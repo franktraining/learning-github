fe41bc2c7068e15507f308ace593f82084e75f6a
Admin <musabaytechcorp@gmail.com>
2018-01-21 02:56:36 -0500

    adding chrome driver, updating pom.xml, adding selenium test

---
+++ GitHubProject/src/test/java/selenium/tests/JenkinsTest.java
@@ -0,0 +1,21 @@
+package selenium.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+
+	WebDriver driver;
+	
+	
+	@Test
+	public void firstTest() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		Thread.sleep(10 * 1000);
+		driver.quit();
+	}
+}
