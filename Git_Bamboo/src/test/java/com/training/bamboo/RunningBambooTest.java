2c19b4e7dbedd6e02646b62af4d749de172d8939
Admin <musabaytechcorp@gmail.com>
2019-09-28 01:31:13 -0400

    adding new project

---
+++ Git_Bamboo/src/test/java/com/training/bamboo/RunningBambooTest.java
@@ -0,0 +1,10 @@
+package com.training.bamboo;
+
+public class RunningBambooTest {
+
+	public static void main(String[] args) {
+		System.out.println("I love learning Java programming !");
+
+	}
+
+}
