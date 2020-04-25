60fd413f696047b052ba64880d8ddb094230a624
Admin <musabaytechcorp@gmail.com>
2020-04-25 16:00:37 -0400

    Jira-C110 adding another sysout line, deleted auto comment

--- GibHubProject2/src/com/learning/git/tests/HellowWorld.java
+++ GibHubProject2/src/com/learning/git/tests/HellowWorld.java
@@ -2,8 +2,8 @@ package com.learning.git.tests;
 
 public class HellowWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 		System.out.println("Hello World !");
+		System.out.println("Learning github is getting interesting!");
 	}
 }
